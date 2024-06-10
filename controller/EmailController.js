b0a5ded5cd538114dfe92d275f8f8570ca887654
tankren <tankrenlive@gmail.com>
2024-06-10 19:37:27 +0800

    增加邮件封装

--- controller/EmailController.js
+++ controller/EmailController.js
@@ -8,14 +8,15 @@ module.exports = {
     let item_num = req.body.item_num || 1; // 数量
     let strong_num = req.body.strong_num || 0; // 强化值
     let coin = req.body.coin || 0; // 金币
+    let seal_flag = req.body.seal || 0;  // 封装
 
     let d = new Date();
-    let datetime = `${d.toLocaleDateString()} ${d.toLocaleTimeString()}`
+    let datetime = `${d.toLocaleDateString('zh-TW')} ${d.toLocaleTimeString()}`  //台湾时间格式，否则为空
 
     let sql = `insert into taiwan_cain_2nd.letter 
-    (charac_no,send_charac_name,letter_text) 
+    (charac_no,send_charac_name,letter_text,reg_date) 
     values 
-    (${mid},'GM','')`;
+    (${mid},'DNF GM','祝您游戏愉快 :D','${datetime}')`;
 
     db(sql)
     .then( result => {
@@ -23,7 +24,7 @@ module.exports = {
       let sql = `insert into taiwan_cain_2nd.postal 
       (occ_time,send_charac_name,receive_charac_no,item_id,add_info,upgrade,amplify_option,amplify_value,gold,seal_flag,letter_id,seperate_upgrade)
        values 
-       ('${datetime}','GM',${mid},${item_id},${item_num},${strong_num},0,0,${coin},0,${insertId}, 0)`;
+       ('${datetime}','DNF GM',${mid},${item_id},${item_num},${strong_num},0,0,${coin},${seal_flag},${insertId}, 0)`;
 
        return db(sql)
     })
