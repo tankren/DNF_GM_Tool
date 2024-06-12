1111fe4a8138161122dd71a69734c4e98744a6df
tankren <tankrenlive@gmail.com>
2024-06-12 20:11:17 +0800

    fix mail

--- controller/CodeController.js
+++ controller/CodeController.js
@@ -25,36 +25,4 @@ module.exports = {
     })
   },
 
-  mailcode: (req, res, next) => {
-    let item_id = req.query.code; // 物品id
-    let mid = req.session.mid; // 角色id
-    console.log("物品代码为：" + item_id);
-    let item_num = 1; // 数量
-    let strong_num = 0; // 强化值
-    
-    let d = new Date();
-    let datetime = `${d.toLocaleDateString('zh-TW')} ${d.toLocaleTimeString()}`  //台湾时间格式，否则为空
-    
-    let sql = `insert into taiwan_cain_2nd.letter 
-    (charac_no,send_charac_name,letter_text,reg_date) 
-    values 
-    (${mid},'DNF GM','祝您游戏愉快 :D','${datetime}')`;
-
-    db(sql)
-    .then( result => {
-      let insertId = result.insertId
-      let sql = `insert into taiwan_cain_2nd.postal 
-      (occ_time,send_charac_name,receive_charac_no,item_id,add_info,upgrade,amplify_option,amplify_value,gold,seal_flag,letter_id,seperate_upgrade)
-       values 
-       ('${datetime}','DNF GM',${mid},${item_id},${item_num},${strong_num},0,0,0,0,${insertId}, 0)`;
-
-       return db(sql)
-    })
-    .then( result => {
-      return res.json({
-        code: 200,
-        msg: "物品发送成功"
-      })
-    })
-  }
 }
